94c68f9ef7f878a60742d14c503833f4ba48b69e
hieucon7438 <hieucon7438@gmail.com>
2024-05-07 22:42:07 +0700

    Config HomeController

--- hanulms/src/main/java/fit/se2/hanulms/controller/HomeController.java
+++ hanulms/src/main/java/fit/se2/hanulms/controller/HomeController.java
@@ -1,4 +1,4 @@
-package fit.se2.blogWebApp.controller;
+package fit.se2.hanulms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "lecturer/course/myCourses";
     }
 }
--- hanulms/src/main/java/fit/se2/hanulms/controller/MemberController.java
+++ hanulms/src/main/java/fit/se2/hanulms/controller/MemberController.java
@@ -1,12 +1,12 @@
-package fit.se2.blogWebApp.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MemberController {
-    @GetMapping("/member")
-    public String member() {
-        return "member";
-    }
-}
+//package fit.se2.hanulms.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class MemberController {
+//    @GetMapping("/member")
+//    public String member() {
+//        return "member";
+//    }
+//}
