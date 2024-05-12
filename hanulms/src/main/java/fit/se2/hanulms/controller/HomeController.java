ef63d16f222d8f89227b62bfa510d6b9631bc092
hieucon7438 <hieucon7438@gmail.com>
2024-05-13 01:42:27 +0700

    Tích hợp Homepage

--- hanulms/src/main/java/fit/se2/hanulms/controller/HomeController.java
+++ hanulms/src/main/java/fit/se2/hanulms/controller/HomeController.java
@@ -13,6 +13,6 @@ import java.util.List;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "redirect:/myCourses";
+        return "/homepage";
     }
 }
