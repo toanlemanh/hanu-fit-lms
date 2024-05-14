5b9816231602f0b94774a95f2065120e0f5e08e6
AltEnter <89359958+ilove-u@users.noreply.github.com>
2024-05-14 23:45:50 +0700

    Delete src/main/java/fit/se2/hanulms/Repository directory

--- src/main/java/fit/se2/hanulms/Repository/SubmissionRepository.java
+++
@@ -1,7 +0,0 @@
-package fit.se2.hanulms.Repository;
-
-import fit.se2.hanulms.model.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
