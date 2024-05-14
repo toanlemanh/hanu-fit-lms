e611f287cfd1d148f1e7aed069a6e2ea8cd43f12
AltEnter <89359958+ilove-u@users.noreply.github.com>
2024-05-14 23:03:28 +0700

    Create FacultyAnnouncementRepository.java

---
+++ hanulms/src/main/java/fit/se2/hanulms/Repository/FacultyAnnouncementRepository.java
@@ -0,0 +1,7 @@
+package fit.se2.hanulms.Repository;
+
+import fit.se2.hanulms.model.FacultyAnnouncement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacultyAnnouncementRepository extends JpaRepository<FacultyAnnouncement, Long> {
+}
